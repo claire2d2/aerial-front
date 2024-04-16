4af04f8fccb0662a6a201784eb0a31d157215287
Claire Song <claireyuan.song@gmail.com>
2024-04-16 16:26:19 +0200

    cleaning up

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,25 +1,4 @@
-import aerialApi from "../service/aerialApi";
-import { useState, useEffect } from "react";
-
 const HomePage = () => {
-  const [health, setHealth] = useState<boolean>(false);
-
-  const fetchHealth = async () => {
-    try {
-      const response = await aerialApi.get(`/health`);
-      console.log(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchHealth();
-  }, []);
-  if (!health) {
-    return <p>Loading</p>;
-  }
-
   return (
     <div>
       {/* {figures &&
