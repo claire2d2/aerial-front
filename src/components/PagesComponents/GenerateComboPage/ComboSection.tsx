3422b403035b164fc4c54d86a38773e641cec9ff
Claire Song <claireyuan.song@gmail.com>
2024-04-23 21:57:18 +0200

    can save generated combo

--- src/components/PagesComponents/GenerateComboPage/ComboSection.tsx
+++ src/components/PagesComponents/GenerateComboPage/ComboSection.tsx
@@ -1,14 +1,23 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import useUser from "../../../context/useUser";
 import { figType } from "../../Types";
 import { Link } from "react-router-dom";
+import aerialApi from "../../../service/aerialApi";
+
+// styling
+import { HiX } from "react-icons/hi";
+const figureStyle =
+  "capitalize bg-white px-4 py-2 text-xl text-text font-semibold w-64 text-center rounded-lg animate-fade bg-opacity-90 hover:text-mainlight";
 
 type ComboSectionProps = {
   generatedCombo: figType[];
 };
+
 const ComboSection: React.FC<ComboSectionProps> = ({ generatedCombo }) => {
-  const [displayedCombo, setDisplayedCombo] = useState<figType[]>([]);
+  // get current discipline to enable user to open figure page in a new window when clicking on figure
   const { currDiscipline } = useUser();
+  // use state + use effect to get to display combo figure by figure
+  const [displayedCombo, setDisplayedCombo] = useState<figType[]>([]);
   useEffect(() => {
     setDisplayedCombo([]);
     const timeoutIds: number[] = [];
@@ -19,14 +28,43 @@ const ComboSection: React.FC<ComboSectionProps> = ({ generatedCombo }) => {
       timeoutIds.push(timeoutId);
     });
 
-    // Cleanup function to clear timeouts on component unmount
+    // cleanup function to clear timeouts on component unmount
     return () => {
       timeoutIds.forEach((timeoutId) => clearTimeout(timeoutId));
     };
   }, [generatedCombo]);
 
-  const figureStyle =
-    "capitalize bg-white px-4 py-2 text-xl text-text font-semibold w-64 text-center rounded-lg animate-fade bg-opacity-90 hover:text-mainlight";
+  // save combo to your combos list
+  const [comboName, setComboName] = useState<string>("");
+
+  function handleNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const givenName = e.currentTarget.value;
+    setComboName(givenName);
+  }
+  // dialog that opens when clicking on save combo
+  const comboModal = useRef<HTMLDialogElement | null>(null);
+
+  function openComboModal() {
+    comboModal.current?.showModal();
+  }
+  function closeComboModal() {
+    comboModal.current?.close();
+  }
+  async function saveCombo() {
+    try {
+      const response = await aerialApi.post("/combos", {
+        name: comboName,
+        discipline: currDiscipline?._id,
+        figures: generatedCombo,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+    closeComboModal();
+  }
+  // toast that shows when combo has been succesfully added (or not)
+
   return (
     <div className="flex flex-col gap-2 justify-center text-gray h-full w-full relative">
       <div className="flex flex-col justify-center items-center lg:gap-10">
@@ -44,10 +82,47 @@ const ComboSection: React.FC<ComboSectionProps> = ({ generatedCombo }) => {
         })}
       </div>
       {displayedCombo.length === generatedCombo.length ? (
-        <button className="absolute top-0 right-0 py-5 px-5">Save Combo</button>
+        <button
+          onClick={openComboModal}
+          className="absolute top-0 right-0 py-5 px-5"
+        >
+          Save Combo
+        </button>
       ) : (
         ""
       )}
+
+      <dialog ref={comboModal} className="rounded-lg drop-shadow-lg">
+        <div className="h-64 w-80  lg:h-64 flex flex-col items-center gap-4 py-5 pt-10">
+          <button
+            onClick={closeComboModal}
+            className="absolute top-1 right-2 hover:text-isFave transition-all"
+          >
+            <HiX />
+          </button>
+          <h3 className="font-bold text-xl">
+            New {currDiscipline?.name} combo:{" "}
+          </h3>
+          <label htmlFor="comboName" className="font-semibold">
+            Give a name to your combo :
+          </label>
+          <input
+            type="text"
+            id="comboName"
+            name="comboName"
+            placeholder="Name..."
+            value={comboName}
+            onChange={handleNameChange}
+            className="rounded-lg drop-shadow-md py-1 px-3"
+          />
+          <button
+            onClick={saveCombo}
+            className="bg-main text-white px-4 py-2 rounded-lg drop-shadow-sm"
+          >
+            Save combo
+          </button>
+        </div>
+      </dialog>
     </div>
   );
 };
