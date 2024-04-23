9bd530ed25acb9c0dee0ec4def8bad64dabaf2d2
Claire Song <claireyuan.song@gmail.com>
2024-04-23 21:30:13 +0200

    search bar for figures page

--- src/components/PagesComponents/AllFiguresPageComponents/SearchBar.tsx
+++ src/components/PagesComponents/AllFiguresPageComponents/SearchBar.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { HiOutlineSearch, HiX } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
+import useUser from "../../../context/useUser";
 
 import { figType } from "../../Types";
 
@@ -11,8 +13,14 @@ const SearchBar: React.FC<SearchBarProps> = ({ placeholder, figures }) => {
   const [searchedFigs, setSearchedFigs] = useState<figType[]>([]);
   const [searchWord, setSearchWord] = useState<string>("");
 
-  function handleSearch(e: React.ChangeEvent<HTMLInputElement>) {
-    setSearchWord(e.currentTarget.value);
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchValue = e.currentTarget.value;
+    setSearchWord(searchValue);
+    console.log(searchWord);
+  };
+
+  // use effect hook to refresh figures search instantaneously
+  useEffect(() => {
     if (searchWord === "") {
       setSearchedFigs([]);
     } else {
@@ -21,16 +29,22 @@ const SearchBar: React.FC<SearchBarProps> = ({ placeholder, figures }) => {
       });
       setSearchedFigs(newFilter);
     }
-  }
+  }, [searchWord]);
 
   function clearSearch() {
     setSearchedFigs([]);
     setSearchWord("");
   }
 
+  const navigate = useNavigate();
+  const { currDiscipline } = useUser();
+  function goToFigPage(figureRef: string) {
+    navigate(`/${currDiscipline?.ref}/figures/${figureRef}`);
+  }
+
   return (
     <div>
-      <div className="searchInput flex items-center gap-2 bg-white">
+      <div className="searchInput flex items-center gap-2 bg-white  drop-shadow-sm px-2 py-1">
         <input
           type="text"
           id="search"
@@ -38,7 +52,7 @@ const SearchBar: React.FC<SearchBarProps> = ({ placeholder, figures }) => {
           placeholder={`${placeholder}`}
           onChange={handleSearch}
         />
-        <label htmlFor="search" className="flex items-center gap-2">
+        <label htmlFor="search" className="flex items-center gap-2 ">
           {searchedFigs.length === 0 ? (
             <div className="hover:cursor-pointer">
               <HiOutlineSearch />
@@ -51,9 +65,17 @@ const SearchBar: React.FC<SearchBarProps> = ({ placeholder, figures }) => {
         </label>
       </div>
       {searchedFigs.length !== 0 && (
-        <div className="searchResults overflow-scroll no-scrollbar h-20">
+        <div className="searchResults overflow-scroll no-scrollbar h-20 flex flex-col">
           {searchedFigs.map((fig, index) => {
-            return <div key={index}>{fig.name}</div>;
+            return (
+              <button
+                onClick={() => goToFigPage(fig.ref)}
+                key={index}
+                className="capitalize hover:bg-bgmainlight"
+              >
+                {fig.name}
+              </button>
+            );
           })}
         </div>
       )}
