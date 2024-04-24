53874d0b4fcaf2720b1a63bd95ec927be2dbdc08
Claire Song <claireyuan.song@gmail.com>
2024-04-24 10:37:03 +0200

    display all combos and show combo chosen

--- src/pages/AllCombos.tsx
+++ src/pages/AllCombos.tsx
@@ -11,6 +11,7 @@ type comboType = {
   comment: string;
 };
 const AllCombos = () => {
+  // get the data from existing combos
   const { currDiscipline } = useUser();
   const [allCombos, setAllCombos] = useState<comboType[]>([]);
 
@@ -28,20 +29,68 @@ const AllCombos = () => {
       console.log(error);
     }
   }
+
+  /* handle showing only the first two figures, and when clicking :
+   ** - the concerned combo will appear in the designated area
+   ** - the full list of figures will show in a flex-col instead of flex-row in the button
+   */
+
+  const [shownCombo, setShownCombo] = useState<comboType | null>(null);
+
+  function choseCombo(combo: comboType) {
+    setShownCombo(combo);
+  }
+
   return (
     <div className="w-full flex flex-col lg:flex-row lg:h-full">
-      <div className="basis-1/4 h-full bg-main text-white">Search</div>
-      <div className="lg:basis-1/2 h-full">
-        <h2>Create a Combo / Edit combo</h2>
-        <div>Upcoming feature</div>
-      </div>
-      <div className="lg:h-full basis-1/4 bg-main">
-        <h2 className="text-white">All combos</h2>
-        <div>
-          {allCombos?.map((combo) => {
-            return <div>{combo.name}</div>;
-          })}
+      <div className="w-full h-72 py-3 lg:h-full lg:w-1/3 bg-main py-2 flex flex-col">
+        <h2 className="text-white font-romantic text-4xl text-center">
+          All combos
+        </h2>
+        <div className="text-white px-3 py-2 text-center">
+          <p>Click on a combo in the list below to view its details.</p>
+          <p>
+            If you wish to edit its content, please click on the edit button
+          </p>
         </div>
+        <div className="overflow-y-scroll bg-white my-2 mx-3">
+          {allCombos
+            ? allCombos.map((combo, index) => {
+                return (
+                  <button
+                    key={index}
+                    onClick={() => choseCombo(combo)}
+                    className="w-full flex flex-col"
+                  >
+                    <h5>{combo.name}</h5>
+                    <div className="flex">
+                      {combo.figures.map((fig, index) => {
+                        return (
+                          <div key={index} className="text-text">
+                            {fig.name}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </button>
+                );
+              })
+            : "Loading"}
+        </div>
+      </div>
+      <div className="lg:w-2/3 h-full flex flex-col items-center">
+        <h2 className="font-romantic text-2xl">
+          {shownCombo ? shownCombo.name : "No combo chosen"}
+        </h2>
+        {shownCombo ? (
+          <div>
+            {shownCombo.figures.map((fig) => {
+              return <div>{fig.name}</div>;
+            })}
+          </div>
+        ) : (
+          <div>Please choose a combo to show</div>
+        )}
       </div>
     </div>
   );
