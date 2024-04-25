9a862c0cbb7d8843484f5543cbaab462091f7113
Claire Song <claireyuan.song@gmail.com>
2024-04-25 16:58:51 +0200

    refactored fave button

--- src/components/GlobalComponents/FavoriteButton.tsx
+++ src/components/GlobalComponents/FavoriteButton.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect, useRef } from "react";
 import aerialApi from "../../service/aerialApi";
 import useUser from "../../context/useUser";
+import { faveType } from "../Types";
+import { fetchFaves } from "../PagesComponents/FiguresFunctions";
 
-import LogInModal from "../GlobalComponents/LogInModal";
+import LogInModal from "./LogInModal";
 import { HiHeart } from "react-icons/hi2";
 
 type FavoriteButtonProps = {
@@ -14,15 +16,20 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({
   fullButton,
   figId,
 }) => {
-  const { favorites, fetchFavorites, isLoggedIn } = useUser();
+  const { isLoggedIn } = useUser();
+  const [faves, setFaves] = useState<faveType[]>([]);
   const [isFave, setIsFave] = useState<boolean>(false);
 
   // check whether figure is already in favorites or not
   useEffect(() => {
-    if (favorites?.find((fave) => fave.figure._id === figId)) {
+    fetchFaves(setFaves);
+  }, [isLoggedIn]);
+
+  useEffect(() => {
+    if (faves?.find((fave) => fave.figure._id === figId)) {
       setIsFave(true);
     }
-  }, []);
+  }, [faves]);
 
   // add or remove figure from favorite when clicking on the button
   async function handleFavorite(e: React.MouseEvent<HTMLElement>) {
@@ -74,7 +81,9 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({
         <button
           onClick={(e) => handleFavorite(e)}
           className={`w-full flex  justify-center gap-2 items-center rounded-lg px-1 py-2 border text-lg font-semibold shadow-sm ${
-            isFave ? "border-gray text-main" : "border-disabled text-gray"
+            isFave
+              ? "border-gray text-main bg-white dark:bg-opacity-60"
+              : "border-disabled text-gray dark:text-gray dark:bg-white dark:bg-opacity-10 "
           }`}
         >
           <HiHeart
